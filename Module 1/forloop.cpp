f1f47a6bca720835f6c8b6731192e5bc8a70d782
jxhn0214 <141537011+jxhn0214@users.noreply.github.com>
2025-06-19 12:50:25 -0700

    Critical Skill 1.9: Two Control Statements (for)
    In this program, I learned how to implement a for loop through the template of for(initialization; condition; increment). I also learned that instead of using "var = var + 1" for the increment, we can just use "var++" because the ++ increases the operand by 1. My program prints out 100 numbers, with every number creating a new line until 100, because the condition was "<= 100."

---
+++ Module 1/forloop.cpp
@@ -0,0 +1,13 @@
+// Program that demonstrates the for loop 
+
+#include <iostream>
+
+int main()
+{
+    int count;
+
+    for(count = 1; count <= 100; count = count++) // intialization at 1, condition up to 100, increments by 1 each iteration
+        std::cout << count << "\n"; // statement prints one number for each line until 100
+    
+    return 0;
+}
